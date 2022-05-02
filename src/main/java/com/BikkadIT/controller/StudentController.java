6cc900138eb94d132388edc256a333b2859a9634
Sagar4971 <sagarnerkar2912@gmail.com>
2022-05-02 11:23:38 +0530

    Added Controller

---
+++ src/main/java/com/BikkadIT/controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.BikkadIT.controller;
+
+import com.BikkadIT.model.Student;
+
+public class StudentController {
+	
+	public static void main(String[] args) {
+		
+	
+	Student stu=new Student();
+	stu.setsId(71);
+	stu.setsName("Sagar");
+	stu.setsAdd("Baglan");
+	stu.setsPhoneNo(94031616);
+	
+	System.out.println(stu);
+	
+	
+	
+	}
+}
